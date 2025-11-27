6ccecb9dde5b1f410508f9d59536a9421aaf5b5d
Todd Hootman <tjhootman@gmail.com>
2025-11-27 16:43:39 -0500

    FEAT: Added symbol and color options for RatingsView

--- CustomRatings/ContentView.swift
+++ CustomRatings/ContentView.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentRating: Int = 0
+    @State private var heartRating: Int = 0
+    @State private var pinRating: Int = 0
     var body: some View {
         RatingsView (
             maxRating: 5,
             currentRating: $currentRating,
             width: 40
+        )
+        RatingsView(maxRating: 3,
+                    currentRating: $heartRating,
+                    width: 50,
+                    symbol: "heart",
+                    color: .red
+        )
+        RatingsView(maxRating: 5,
+                    currentRating: $pinRating,
+                    width: 40,
+                    symbol: "pin",
+                    color: .blue
         )
     }
 }
--- CustomRatings/RatingsView.swift
+++ CustomRatings/RatingsView.swift
@@ -4,28 +4,37 @@ struct RatingsView: View {
     let maxRating: Int
     @Binding var currentRating: Int
     let width: Int
+    let symbol: String
+    let color: Color
     
-    init(maxRating: Int, currentRating: Binding<Int>, width: Int = 30) {
+    init(maxRating: Int,
+         currentRating: Binding<Int>,
+         width: Int = 30,
+         symbol: String = "star",
+         color: Color = .yellow
+    ) {
         self.maxRating = maxRating
         self._currentRating = currentRating
         self.width = width
+        self.symbol = symbol
+        self.color = color
     }
     var body: some View {
         HStack {
-            Image(systemName: "star")
+            Image(systemName: symbol)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.yellow)
+                .foregroundStyle(color)
                 .symbolVariant(.slash)
                 .opacity(currentRating == 0 ? 0 : 1)
                 .onTapGesture {
                     currentRating = 0
                 }
             ForEach(0..<maxRating, id: \.self) { rating in
-                Image(systemName: "star")
+                Image(systemName: symbol)
                     .resizable()
                     .scaledToFit()
-                    .foregroundStyle(.yellow)
+                    .foregroundStyle(color)
                     .symbolVariant(rating < currentRating ? .fill : .none)
                     .onTapGesture {
                         withAnimation {
