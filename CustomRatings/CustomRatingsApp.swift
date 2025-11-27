af2da0ecee147ab9344e9efc715d2c2b170ff31a
Todd Hootman <tjhootman@gmail.com>
2025-11-27 15:17:18 -0500

    Initial Commit

---
+++ CustomRatings/CustomRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRatingsApp.swift
+//  CustomRatings
+//
+//  Created by Todd Hootman on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
